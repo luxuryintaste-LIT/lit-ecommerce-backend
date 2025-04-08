05ce97ddc2aa90053d9eaa0e2ad24fdd1d34e0a2
MohammedSaif05 <pianist.saif05@gmail.com>
2025-04-08 14:28:30 +0530

    fixed again

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,56 +10,51 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-left">
           <img src={logo} alt="LuxuryInTaste Logo" className="footer-logo" />
-        </div>
-        
-        <div className="footer-nav">
-          <nav className="footer-links">
+          <div className="footer-links">
             <a href="/" className="footer-link">Home</a>
             <a href="/about" className="footer-link">About</a>
             <a href="/privacy" className="footer-link">Privacy Policy</a>
             <a href="/terms" className="footer-link">Terms of Services</a>
             <a href="/contact" className="footer-link">Contact Us</a>
-          </nav>
-        </div>
-
-        <div className="footer-subscribe">
-          <h3>Join our Community</h3>
-          <div className="subscribe-form">
-            <input 
-              type="email" 
-              placeholder="Enter Your Email" 
-              className="subscribe-input"
-            />
-            <button className="subscribe-button">Subscribe</button>
           </div>
         </div>
 
-        <div className="footer-social">
+        <div className="footer-middle">
+          <div className="footer-subscribe">
+            <h3>Join our Community</h3>
+            <div className="subscribe-form">
+              <input 
+                type="email" 
+                placeholder="Enter Your Email" 
+                className="subscribe-input"
+              />
+              <button className="subscribe-button">Subscribe</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer-right">
           <div className="social-links">
-            <a href="mailto:contact@luxuryintaste.com" className="social-link">
+            <div className="social-link">
               <span>Email</span>
               <i className="fas fa-envelope"></i>
-            </a>
-            <a href="https://linkedin.com" className="social-link">
+            </div>
+            <div className="social-link">
               <span>Linkedin</span>
               <i className="fab fa-linkedin"></i>
-            </a>
-            <a href="https://instagram.com" className="social-link">
+            </div>
+            <div className="social-link">
               <span>Instagram</span>
               <i className="fab fa-instagram"></i>
-            </a>
-            <a href="https://twitter.com" className="social-link">
+            </div>
+            <div className="social-link">
               <span>Twitter</span>
               <i className="fab fa-twitter"></i>
-            </a>
+            </div>
           </div>
           <div className="app-downloads">
-            <a href="#" className="app-download-link">
-              <img src={googlePlay} alt="Get it on Google Play" />
-            </a>
-            <a href="#" className="app-download-link">
-              <img src={appStore} alt="Download on the App Store" />
-            </a>
+            <img src={googlePlay} alt="Get it on Google Play" className="store-badge" />
+            <img src={appStore} alt="Download on the App Store" className="store-badge" />
           </div>
         </div>
       </div>

1eb3e00878030d20263a66a38d963f8af130468d
MohammedSaif05 <pianist.saif05@gmail.com>
2025-04-09 15:22:27 +0530

    fixed the search bar animation

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/SearchBar.css';
 
 const SearchBar = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+  
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+  
+  const handleBlur = () => {
+    if (!inputValue) {
+      setIsFocused(false);
+    }
+  };
+  
   return (
     <div className="search-container">
       <div className="search-wrapper">
-        <div className="search-box">
+        <div className={`search-box ${isFocused || inputValue ? 'has-text' : ''}`}>
           <input 
             type="text" 
             placeholder="Search for products..." 
             className="search-input"
+            value={inputValue}
+            onChange={handleInputChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
           />
           <button className="search-button">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">

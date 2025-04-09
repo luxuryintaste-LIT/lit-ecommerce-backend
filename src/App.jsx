428554c3d15100c1feb4ec79310c4190ab869bb5
MohammedSaif05 <pianist.saif05@gmail.com>
2025-04-09 10:53:27 +0530

    restored the old code..

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import SearchBar from './components/SearchBar';
 import CategoryCards from './components/CategoryCards';
+import SearchBar from './components/SearchBar';
 import FilterBar from './components/FilterBar';
-import ProductDetails from './components/ProductDetails';
-import Home from './pages/Home';
+import ProductList from './components/ProductList';
+import Footer from './components/Footer';
+import bodyBg from './img/body-bg.png';
 import './App.css';
 
 function App() {
@@ -17,14 +17,11 @@ function App() {
         <SearchBar />
         <CategoryCards />
         <FilterBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
-        </Routes>
+        <ProductList />
         <Footer />
       </div>
     </Router>
   );
-}
+} 
 
 export default App;

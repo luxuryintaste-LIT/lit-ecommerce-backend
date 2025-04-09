b652a8507a2767d301e33b8607760046eafacf32
MohammedSaif05 <pianist.saif05@gmail.com>
2025-04-09 10:34:48 +0530

    again i have fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import SearchBar from './components/SearchBar';
+import CategoryCards from './components/CategoryCards';
+import FilterBar from './components/FilterBar';
+import ProductList from './components/ProductList';
+import ProductDetails from './components/ProductDetails';
 import Home from './pages/Home';
-import './styles/App.css';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Navbar />
+        <SearchBar />
+        <CategoryCards />
+        <FilterBar />
+        <ProductList />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
         </Routes>
         <Footer />
       </div>

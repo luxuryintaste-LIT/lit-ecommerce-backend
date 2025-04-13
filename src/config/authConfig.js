0c7cdebab4277be7f6950d4a662f43da008d5e60
MohammedSaif05 <pianist.saif05@gmail.com>
2025-04-13 22:14:19 +0530

    tried fix for netlify url

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -2,7 +2,9 @@ export const msalConfig = {
   auth: {
     clientId: "8ff0729a-f97c-4c76-ac29-9845cf98ffda", // Replace with your Azure AD app registration client ID
     authority: "https://login.microsoftonline.com/2c355bd8-c53c-4181-81de-5d370bbc8405", // Replace with your tenant ID
-    redirectUri: window.location.origin + "/admin/login", // The redirect URI registered in Azure AD
+    redirectUri: process.env.NODE_ENV === 'production' 
+      ? "https://lit-ecommerce.netlify.app/admin/login" 
+      : window.location.origin + "/admin/login", // Use Netlify URL in production, local URL in development
   },
   cache: {
     cacheLocation: "sessionStorage",

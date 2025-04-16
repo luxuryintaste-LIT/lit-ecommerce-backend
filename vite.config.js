1159a18d5d8c512fdc0f8a7b2797835255b7305f
MohammedSaif05 <pianist.saif05@gmail.com>
2025-04-16 12:01:05 +0530

    added azure config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Add Buffer polyfill
+    'process.env': {},
+    'global': {},
+    'Buffer': ['buffer', 'Buffer'],
+  },
+  resolve: {
+    alias: {
+      // Add buffer polyfill
+      'buffer': 'buffer/',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
 })

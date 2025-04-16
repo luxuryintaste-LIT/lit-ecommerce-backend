d6942b3014dca7b12e9281d6865695aa0605522d
MohammedSaif05 <pianist.saif05@gmail.com>
2025-04-16 12:12:42 +0530

    again fixed

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,23 @@ import 'stream-browserify';
 import 'util';
 import 'assert';
 import 'events';
+import 'crypto-browserify';
+import 'url-browserify';
+import 'querystring-es3';
+import 'stream-http';
+import 'https-browserify';
+import 'os-browserify/browser';
+import 'path-browserify';
+import 'browserify-fs';
+import 'browserify-zlib';
 
 // Add polyfills to window
 window.Buffer = Buffer;
 window.process = process;
 window.global = window;
+
+// Add URL polyfill
+window.URL = window.URL || window.webkitURL;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
